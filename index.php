1f7e733c1dc9204052c9c2aa68bfc8a5edfec4f2
EricVanDeGriesel <55184120+EricVanDeGriesel@users.noreply.github.com>
2020-11-27 22:42:37 +0100

    icons

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+        <link rel="icon" type="image/png" href="/assets/img/icon.svg">
         <link rel="stylesheet" href="assets/css/app.css">
         <link rel="stylesheet" href="assets/css/prop.css">
 
