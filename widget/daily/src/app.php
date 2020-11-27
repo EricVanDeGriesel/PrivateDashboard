bdf94d325399244f78a51bc27fec422551510412
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2020-11-28 00:24:04 +0100

    blank widget prep

--- widget/daily/src/app.php
+++ widget/daily/src/app.php
@@ -1,32 +1,5 @@
 <div class="col-12">
-    <div class="row">
-        <div class="col-12 col-md-8 col-lg-6 my-3">
-            <div class="h-100 p-4 bg-dark rounded-30 d-flex justify-content-center align-items-center">
-                <p id="clock" class="display-3 mb-0">
-                    00:00:00
-                </p>
-            </div>
-        </div>
-
-        <div class="col-5 col-md-4 col-lg-2 my-3">
-            <div class="h-100 p-4 bg-dark rounded-30 d-flex justify-content-center align-items-center">
-                <div class="text-left">
-                    <p id="weekday" class="text-danger text-uppercase mb-1">
-                        Tag
-                    </p>
-                    <p id="dateday" class="display-4 mb-0">
-                        00
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-7 col-md-12 col-lg-4 my-3">
-            <div class="h-100 p-4 bg-dark rounded-30 d-flex justify-content-center align-items-center">
-                <div class="text-left"></div>
-                    <p class="mb-0">Hier ist noch Platz für mehr...</p>
-                </div>
-            </div>
-        </div>
+    <div class="h-100 p-4 bg-dark rounded-30 d-flex justify-content-center align-items-center">
+        <p>Suche dir in der Layout Branche die passenden Sachen raus.</p>
     </div>
 </div>
