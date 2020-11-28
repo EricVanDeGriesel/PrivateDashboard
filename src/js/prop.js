520ad49cefb9c7d5a03d98585c627c18afa86b6d
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2020-11-28 16:10:05 +0100

    Prop changes and Modal
    Changes the prop function and add a function prop.mousemove. Also added a Modal for the settings.

--- src/js/prop.js
+++ src/js/prop.js
@@ -1,13 +1,5 @@
-const propContainer = document.createElement('div');
-
-propContainer.id = 'propContainer';
-
-document.body.appendChild(propContainer);
-
-function propRain() {
-    const prop = document.createElement('div');
-    const img = document.createElement('img');
-    var props = {
+var prop = {
+    props: {
         0: {
             src: './assets/icons/dollar.svg',
             minSize: 10,
@@ -16,28 +8,105 @@ function propRain() {
             src: './assets/icons/cactus.png',
             minSize: 25,
         },
-    };
-    var currentProp = props[Math.floor(Math.random() * Object.keys(props).length)];
+    },
+    container: () => {
+        const propContainer = document.createElement('div');
+        propContainer.id = 'propContainer';
+        document.body.appendChild(propContainer);
+    },
 
-    prop.classList.add('prop');
-    prop.style.left = Math.random() * 100 + 'vw';
-    prop.style.transform = 'rotate(' + getRandomInt(-50, 50)  + 'deg)';
-    prop.style.animationDuration = Math.random() * 3 + 8 + 's';
-    img.width = Math.random() * 25 + currentProp.minSize;
-    img.src = currentProp.src;
+    /**
+     * Create random generated props that fall from the page top to bottom
+     */
+    rain: () => {
+        prop.container();
 
-    prop.appendChild(img);
-    document.getElementById('propContainer').appendChild(prop);
+        let render = () => {
+            const propElem = document.createElement('div');
+            const img = document.createElement('img');
+            var currentProp = prop.props[Math.floor(Math.random() * Object.keys(prop.props).length)];
 
-    setTimeout(function() {
-        prop.remove();
-    }, 10000);
+            propElem.classList.add('prop-rain');
+            propElem.style.left = Math.random() * 100 + 'vw';
+            propElem.style.transform = 'rotate(' + getRandomInt(-50, 50)  + 'deg)';
+            propElem.style.animationDuration = Math.random() * 3 + 8 + 's';
+            img.width = Math.random() * 25 + currentProp.minSize;
+            img.src = currentProp.src;
+
+            propElem.appendChild(img);
+            document.getElementById('propContainer').appendChild(propElem);
+
+            setTimeout(() => {
+                propElem.remove();
+            }, 10000);
+        }
+
+        let getRandomInt = (min, max) => {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+
+        setInterval(render, Math.random() * 600 + 200);
+    },
+
+    /**
+     * Create random generated props that move on mousemovement
+     */
+    mousemove: () => {
+        prop.container();
+
+        let render = () => {
+            window.propMouseoverloop = getRandomInt(30, 40);
+
+            for (let i = 0; i < window.propMouseoverloop; i++) {
+                const propElem = document.createElement('div');
+                const img = document.createElement('img');
+                var currentProp = prop.props[Math.floor(Math.random() * Object.keys(prop.props).length)];
+
+                propElem.classList.add('prop-mousemove');
+                propElem.classList.add('prop-mousemove-' + i);
+                propElem.setAttribute('move-randomize', Math.random() * 10);
+                propElem.style.top = Math.random() * 100 + 'vh';
+                propElem.style.left = Math.random() * 100 + 'vw';
+                propElem.style.transform = 'rotate(' + getRandomInt(-50, 50)  + 'deg)';
+                img.width = Math.random() * 25 + currentProp.minSize;
+                img.src = currentProp.src;
+
+                propElem.appendChild(img);
+                document.getElementById('propContainer').appendChild(propElem);
+            }
+
+            document.body.addEventListener('mousemove', (e) => {
+                const ww = window.innerWidth;
+                const wh = window.innerHeight;
+                const cx = e.clientX;
+                const cy = e.clientY;
+
+                var calc = (inner, client) => {
+                    //return Math.floor((client - (inner / 2)) / (inner * 0.1));
+                    return (client - (inner / 2)) / (inner * 0.05);
+                }
+    
+                for (let i = 0; i < window.propMouseoverloop; i++) {
+                    const propElem = document.getElementsByClassName('prop-mousemove-' + i)[0];
+                    const x = calc(ww, cx) * (propElem.getAttribute('move-randomize') / 5.5);
+                    const y = calc(wh, cy) * (propElem.getAttribute('move-randomize') / 5.5);
+
+                    propElem.style.transform = 'translateX(' + x + 'px) translateY(' + y + 'px)';
+                }
+            });
+        }
+
+        let getRandomInt = (min, max) => {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+
+        render();
+    }
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-setInterval(propRain, Math.random() * 600 + 200);
